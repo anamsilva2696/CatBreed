60584da354f8f28e7458c7ae2925b013dea18df6
Admin <admin@MBP-Admin.Home>
2024-08-31 17:14:10 +0300

    initial commit

---
+++ CatBreed/CatBreedApp.swift
@@ -0,0 +1,20 @@
+//
+//  CatBreedApp.swift
+//  CatBreed
+//
+//  Created by Admin on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatBreedApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
